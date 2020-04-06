d22d2e5073fcaab68d8c6319c74b6bea6fe64428
Saarthak Sangamnerkar <saarthaksangamnerkar@gmail.com>
2020-04-06 11:21:34 +0530

    string_rev

---
+++ Arrays/string_reverse.js
@@ -0,0 +1,21 @@
+//method 1 - through reverse iteration of string as an array
+function string_reverse1(str) {
+    if(!str || str.length < 2 || typeof str!= 'string')
+        console.log("Invalid Input. Please input a string.");
+    
+    const backwards = []
+    const totalItem = str.length - 1
+    for(let i = totalItem; i >= 0; i --) {
+        backwards.push(str[i])
+
+    }
+    console.log(backwards.join(''));                
+} 
+
+//method 2 - using built-in reverse() function
+let string_reverse2 = (str) => console.log(str.split('').reverse().join(''));
+
+
+
+string_reverse1("Hello")
+string_reverse2("World")
