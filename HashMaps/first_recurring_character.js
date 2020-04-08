15361fc319d721eff7f026894e12a96408dfdd73
Saarthak Sangamnerkar <saarthaksangamnerkar@gmail.com>
2020-04-08 10:59:55 +0530

    improved first_rec_char

--- HashMaps/first_recurring_character.js
+++ HashMaps/first_recurring_character.js
@@ -4,18 +4,29 @@
 //Given an array = [1, 2, 3, 4, 5]
 //it should return undefined
 
-function first_rec_char(arr) {
+function first_rec_char_1(arr) {
     console.clear()
     for (let i = 0; i < arr.length; i++) {
         for (let j = i+1; j < arr.length; j++) {
             if(arr[i] === arr[j]) {
                 return arr[i]
-
             }
         }
     }
     return undefined
-} //O(n^2)
+} //O(n^2) //doesnt work for [2,5,5,1,2,6,7,5] ; this function gives output 2
 
+function first_rec_char_2(arr) {
+    let map = {}
+    for (let i = 0; i < arr.length; i++) {
+        if(map[arr[i]] !== undefined) {
+            return arr[i]
+        } else {
+            map[arr[i]] = i
+        }
+    }
+    return undefined
+} //O(n)
 
-console.log(first_rec_char([2, 5, 1, 2, 6, 7, 5]))
+console.log(first_rec_char_1([2, 5, 1, 2, 6, 7, 5]))
+console.log(first_rec_char_2([2, 5, 5, 1, 2, 6, 7, 8]))
